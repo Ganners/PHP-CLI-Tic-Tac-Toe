5719f574df4f428f957a28113766582d6388e895
Mark Gannaway <mark@ganners.co.uk>
2013-05-25 15:47:33 +0100

    Added doc comments to input handler

--- src/Application/Util/Input_Handler.php
+++ src/Application/Util/Input_Handler.php
@@ -6,10 +6,22 @@ use Application\App_Exception;
 
 class Input_Handler {
 
+    /**
+     * Our construct
+     */
     public function __construct() {
 
     }
 
+    /**
+     * Converts a string to an array suitible for the
+     * input of a board size, i.e. length of 2
+     * 
+     * @param  string $input
+     * @return array
+     * 
+     * @throws App_Exception if array is not greater than 2
+     */
     public function toBoardSizeArray($input) {
         $array = explode(',', $input);
 
@@ -28,6 +40,14 @@ class Input_Handler {
 
     }
 
+    /**
+     * Validates and returns a trimmed string
+     * 
+     * @param  string $input
+     * @return string (trimmed)
+     * 
+     * @throws App_Exception if input is not string or is null
+     */
     public function toString($input) {
 
         if(is_string($input) && $input)
@@ -37,6 +57,14 @@ class Input_Handler {
 
     }
 
+    /**
+     * Converts strings of y/n/yes/no to a bool
+     * 
+     * @param  string $input
+     * @return bool
+     * 
+     * @throws App_Exception if input is not y/n/yes/no
+     */
     public function toBool($input) {
 
         if($input === 'y' || $input === 'yes')
