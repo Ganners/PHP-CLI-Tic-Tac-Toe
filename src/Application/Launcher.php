a3e6da9e5e90e68eb3048fab99c5fd9f33aa565b
Mark Gannaway <mark@ganners.co.uk>
2013-05-25 16:05:51 +0100

    Put in board creation code

--- src/Application/Launcher.php
+++ src/Application/Launcher.php
@@ -4,6 +4,7 @@ namespace Application;
 
 use Application\App_Exception;
 use Application\Util\Input_Handler;
+use Application\Board;
 
 class Launcher {
 
@@ -12,6 +13,38 @@ class Launcher {
               $_outputsSent    = 0,
               $_startupDataGathered = FALSE,
               $_inputHandler;
+
+    protected $_board,
+              $_player1,
+              $_player2;
+
+    protected $_sendReceive = array(
+                array(
+                    'output_message'  => 'Welcome to the game, please input your board size (x,y): ',
+                    'handler_method'  => 'toBoardSizeArray',
+                    'set_as_variable' => '_boardDimensions'
+                ),
+                array(
+                    'output_message'  => 'What is the name of player 1: ',
+                    'handler_method'  => 'toString',
+                    'set_as_variable' => '_player1Name'
+                ),
+                array(
+                    'output_message'  => 'What is the name of player 2: ',
+                    'handler_method'  => 'toString',
+                    'set_as_variable' => '_player2Name'
+                ),
+                array(
+                    'output_message'  => 'Is player 1 a bot? (y/n): ',
+                    'handler_method'  => 'toBool',
+                    'set_as_variable' => '_player1Bot'
+                ),
+                array(
+                    'output_message'  => 'Is player 2 a bot? (y/n): ',
+                    'handler_method'  => 'toBool',
+                    'set_as_variable' => '_player2Bot'
+                ),
+            );
 
     /**
      * Public constructor
@@ -33,11 +66,13 @@ class Launcher {
         }
 
         //Get the game rolling!
-        
+        if($this->_setupBoard() && $this->_setupPlayers()) {
+            // While the application is not terminated
+            while(!$this->_terminate) {
 
-        // While the application is not terminated
-        while(!$this->_terminate) {
-
+            }
+        } else {
+            $this->run();
         }
 
     }
@@ -45,15 +80,40 @@ class Launcher {
     /**
      * Sets up our game board based on input
      */
-    protected function setupBoard() {
-        
+    protected function _setupBoard() {
+        try {
+            $this->_board = new Board(
+                (int) $this->_boardDimensions[0],
+                (int) $this->_boardDimensions[1]
+                );
+
+            return TRUE;
+        } catch(App_Exception $e) {
+
+            //On exception, restart the application
+            echo $e->getMessage() . PHP_EOL;
+            $this->_resetStartupData();
+
+            return FALSE;
+        }
     }
 
     /**
      * Sets up our players based on input
      */
-    protected function setupPlayers() {
-        
+    protected function _setupPlayers() {
+        return TRUE;
+    }
+
+    protected function _resetStartupData() {
+        foreach($this->_sendReceive as $key => $value) {
+            if(isset($this->{$value['set_as_variable']}))
+                unset($this->{$value['set_as_variable']});
+        }
+        $this->_inputsReceived = 0;
+        $this->_outputsSent = 0;
+        $this->_startupDataGathered = FALSE;
+        $this->_board = NULL;
     }
 
     /**
@@ -65,37 +125,9 @@ class Launcher {
          * Our input output array, this is looped through to set the outputs
          * and gather the data for the application.
          */
-        $sendReceive = array(
-            array(
-                'output_message'  => 'Welcome to the game, please input your board size (x,y): ',
-                'handler_method'  => 'toBoardSizeArray',
-                'set_as_variable' => '_boardDimensions'
-            ),
-            array(
-                'output_message'  => 'What is the name of player 1: ',
-                'handler_method'  => 'toString',
-                'set_as_variable' => '_player1Name'
-            ),
-            array(
-                'output_message'  => 'What is the name of player 2: ',
-                'handler_method'  => 'toString',
-                'set_as_variable' => '_player2Name'
-            ),
-            array(
-                'output_message'  => 'Is player 1 a bot? (y/n): ',
-                'handler_method'  => 'toBool',
-                'set_as_variable' => '_player1Bot'
-            ),
-            array(
-                'output_message'  => 'Is player 2 a bot? (y/n): ',
-                'handler_method'  => 'toBool',
-                'set_as_variable' => '_player2Bot'
-            ),
-        );
-
-        while($this->_inputsReceived < count($sendReceive)) {
+        while($this->_inputsReceived < count($this->_sendReceive)) {
             if($this->_outputsSent === $this->_inputsReceived) {
-                echo $sendReceive[$this->_outputsSent]['output_message'];
+                echo $this->_sendReceive[$this->_outputsSent]['output_message'];
                 ++$this->_outputsSent;
             } else {
                 $line = trim(fgets(STDIN));
@@ -103,7 +135,7 @@ class Launcher {
                     try {
                         //Handler will throw an exception if invalid
                         $handledInput = 
-                            $this->_inputHandler->{$sendReceive[$this->_inputsReceived]['handler_method']}($line);
+                            $this->_inputHandler->{$this->_sendReceive[$this->_inputsReceived]['handler_method']}($line);
                     } catch(App_Exception $e) {
 
                         // If we get an exception, display the message and rewind the output
@@ -114,7 +146,7 @@ class Launcher {
                     }
 
                     //Set variable to an object var if it's all good
-                    $this->{$sendReceive[$this->_inputsReceived]['set_as_variable']} = $line;
+                    $this->{$this->_sendReceive[$this->_inputsReceived]['set_as_variable']} = $handledInput;
                     ++$this->_inputsReceived;
                 }
             }
